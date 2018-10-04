24ad29a573f0665a146633f99f9487ed330c54a1
Michal Vlasák <lahcim8@gmail.com>
2018-10-04 14:04:56 +0200

    firefox: initial

---
+++ .mozilla/firefox/profile/user.js
@@ -0,0 +1,19 @@
+user_pref("extensions.pocket.enabled", false);
+user_pref("reader.parse-on-load.enabled", false);
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
+user_pref("browser.search.context.loadInBackground", true);
+user_pref("browser.tabs.loadBookmarksInBackground", true);
+user_pref("view_source.editor.external", true);
+user_pref("view_source.editor.path", "vim");
+user_pref("browser.fullscreen.autohide", false);
+user_pref("geo.enabled", false);
+user_pref("layout.word_select.eat_space_to_next_word", true);
+//potential problems
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("findbar.highlightAll", true);
+user_pref("network.IDN_show_punycode", true);
+user_pref("browser.tabs.insertAfterCurrent", true);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("toolkit.cosmeticAnimations.enabled", false);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
