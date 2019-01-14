d27f5f851133e2b7b1adcdac33d79928321f31ce
Michal Vlasák <lahcim8@gmail.com>
2019-01-14 13:00:31 +0100

    firefox: add font settings and slight reorder

--- .mozilla/firefox/profile/user.js
+++ .mozilla/firefox/profile/user.js
@@ -6,7 +6,17 @@ user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.fullscreen.autohide", false);
 user_pref("geo.enabled", false);
 user_pref("layout.word_select.eat_space_to_next_word", true);
-//potential problems
+
+// Fonts
+user_pref("font.default.x-western", "sans-serif");
+user_pref("font.name.serif.x-western", "serif");
+user_pref("font.name.sans-serif.x-western", "sans-serif");
+user_pref("font.name.monospace.x-western", "monospace");
+
+// Scaling
+user_pref("devPixelsPerPx", "1.0");
+
+// Potential problems
 user_pref("dom.event.clipboardevents.enabled", false);
 user_pref("findbar.highlightAll", true);
 user_pref("network.IDN_show_punycode", true);
@@ -15,4 +25,3 @@ user_pref("browser.tabs.closeWindowWithLastTab", true);
 user_pref("toolkit.cosmeticAnimations.enabled", false);
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
-user_pref("devPixelsPerPx", "1.0");
