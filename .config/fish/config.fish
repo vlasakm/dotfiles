bf87bcbd7f03621a4a8bb5f019714dbf2c745580
Michal Vlasák <lahcim8@gmail.com>
2019-06-29 22:31:13 +0200

    go: move gopath

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,6 +46,7 @@ if status is-login
 	set -x NNN_USE_EDITOR "1"
 	set -x PASSWORD_STORE_CLIP_TIME "3"
 	set -x LESS "-R"
+	set -x GOPATH ~/.local/share/go
 
 	# Address sanitizer options
 	set -x ASAN_OPTIONS  "symbolize=1:debug=1:detect_leaks=1:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1"
