abcff59048815f8f73cb8a1714ffa09476107e6d
Michal Vlasák <lahcim8@gmail.com>
2019-09-14 18:29:47 +0200

    fish: alias for ffmpeg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ if status is-interactive
 	alias grep 'grep --color=auto'
 	alias diff 'diff --color=auto'
 	alias info 'info --vi-keys'
+	alias ffmpeg 'ffmpeg -hide_banner'
 	alias la 'ls -lA'
 	alias ll 'ls -l'
 	alias ls 'ls -hN --color=auto --group-directories-first'
