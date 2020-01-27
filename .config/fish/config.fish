aebcfe5292b1f86c00de40b7fe6764c5742294e7
Michal Vlasák <lahcim8@gmail.com>
2020-01-27 13:34:30 +0100

    fish: set control variables in config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,7 +34,36 @@ if status is-interactive
 	abbr -a -g gsh 'git show'
 	abbr -a -g ytdl 'youtube-dl'
 
+	set fish_greeting ''
+	set fish_key_bindings fish_default_key_bindings
 	set fish_prompt_pwd_dir_length 0
+
+	# Dracula colorscheme
+	set fish_color_autosuggestion BD93F9
+	set fish_color_cancel -r
+	set fish_color_command F8F8F2
+	set fish_color_comment 6272A4
+	set fish_color_cwd green
+	set fish_color_cwd_root red
+	set fish_color_end 50FA7B
+	set fish_color_error FFB86C
+	set fish_color_escape 00a6b2
+	set fish_color_history_current --bold
+	set fish_color_host normal
+	set fish_color_match --background=brblue
+	set fish_color_normal normal
+	set fish_color_operator 00a6b2
+	set fish_color_param FF79C6
+	set fish_color_quote F1FA8C
+	set fish_color_redirection 8BE9FD
+	set fish_color_search_match 'bryellow'  '--background=brblack'
+	set fish_color_selection 'white'  '--bold'  '--background=brblack'
+	set fish_color_user brgreen
+	set fish_color_valid_path --underline
+	set fish_pager_color_completion normal
+	set fish_pager_color_description 'B3A06D'  'yellow'
+	set fish_pager_color_prefix 'white'  '--bold'  '--underline'
+	set fish_pager_color_progress 'brwhite'  '--background=cyan'
 
 	function fish_prompt
 		if test $status -eq 0; set status_color green; else; set status_color red; end
