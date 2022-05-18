2a84fc93e21d0dc90837cb76e4d1cdbfdc55c951
Michal Vlasák <lahcim8@gmail.com>
2022-05-18 14:01:38 +0200

    ipython: fix deprecated magic usage

--- .ipython/profile_default/startup/startup.py
+++ .ipython/profile_default/startup/startup.py
@@ -3,5 +3,5 @@ import sys
 sys.dont_write_bytecode = True
 
 ipython = get_ipython()
-ipython.magic("%reload_ext autoreload")
-ipython.magic("%autoreload 2")
+ipython.run_line_magic("reload_ext", "autoreload")
+ipython.run_line_magic("autoreload", 2)
