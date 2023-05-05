51aedbfc8b7476da85719cb5f5d0ef5a7fc3a7ae
Michal Vlasák <lahcim8@gmail.com>
2023-05-05 13:58:28 +0200

    ipython: Disable autoreload magic
    It doesn't work for quite some time now.

--- .ipython/profile_default/startup/startup.py
+++ .ipython/profile_default/startup/startup.py
@@ -2,6 +2,6 @@ import sys
 
 sys.dont_write_bytecode = True
 
-ipython = get_ipython()
-ipython.run_line_magic("reload_ext", "autoreload")
-ipython.run_line_magic("autoreload", 2)
+#ipython = get_ipython()
+#ipython.run_line_magic("reload_ext", "autoreload")
+#ipython.run_line_magic("autoreload", 2)
